7b3e3680afb25e23add0fa963f85e2255303aa8a
ibooM <iboomblog@gmail.com>
2025-04-22 17:02:03 +0700

    assignment

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let priceProduct = 27000
+let discountPercent = 25/100
+let totalPrice = priceProduct-(priceProduct*discountPercent)
+console.log(totalPrice)
